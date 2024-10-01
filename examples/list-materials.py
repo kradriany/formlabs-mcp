e25e655d9c08f22ccd2cc06f6dd4fa3025e2e599
Jacob Haip <jhaip@users.noreply.github.com>
2024-10-01 14:57:11 -0400

    Add list-materials.py example

---
+++ examples/list-materials.py
@@ -0,0 +1,21 @@
+import formlabs_local_api_minimal as formlabs
+import requests
+import pathlib
+import sys
+import json
+
+pathToPreformServer = None
+if sys.platform == 'win32':
+    pathToPreformServer = pathlib.Path().resolve() / "PreFormServer.exe"
+elif sys.platform == 'darwin':
+    pathToPreformServer = pathlib.Path().resolve() / "PreFormServer.app/Contents/MacOS/PreFormServer"
+else:
+    print("Unsupported platform")
+    sys.exit(1)
+with formlabs.PreFormApi.start_preform_server(pathToPreformServer=pathToPreformServer):
+    response = requests.request("GET", "http://localhost:44388/list-materials/")
+    response.raise_for_status()
+    json_result = response.json()
+    with open("formlabs-materials-data.json", "w") as file:
+        json.dump(json_result, file, indent=4, sort_keys=True)
+        print("Materials data saved to formlabs-materials-data.json")
